01f058022c9bad536f188793ae95317c4fe4cd43
Abdellahi <emmeynabdillah@gmail.com>
2024-02-11 20:43:04 +0000

    add components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
 export default function App() {
   console.log("yes");
-  return <h1 className="text-green-600">begin</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
